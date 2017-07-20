dc48e000c032d106e585a2ab85b74ca5a6b79643
junjun <1121968314@qq.com>
2017-07-19 23:37:28 -0400

    ..

--- main.js
+++ main.js
@@ -1,47 +1,25 @@
-'use strict'
-
-function guessNumber(num) {
-    let testNumber = 1234;
-    //let testNumber = Math.floor(Math.random() * 9999 + 1000);
-    let number=testNumber.toString();
+'use strict';
+const readlineSync = require('readline-sync');
+function guessNumber() {
+    const randomNumber = this.getRandomNumber();
+    const inputGuessNumber=readlineSync.question(`the testNumber is ${randomNumber},please input the num is:`);
     let countA = 0;
-    let countB = 0;
-    for(let i in num){
-        if (num[i]===number[i]){
+    let countAll = 0;
+    for(let index in randomNumber){
+        if(inputGuessNumber.indexOf(randomNumber[index])!==-1){
+            countAll++;
+        }
+        if(randomNumber[index]===inputGuessNumber[index]){
             countA++;
         }
-        else {
-            for (let j in number){
-                if (num[i]===number[j]){
-                    countB++;
-                }
-            }
-        }
-    }
-
+    let countB=countAll-countA;
     return `${countA}A${countB}B`;
+    }
 };
-module.exports.guessNumber=guessNumber();
-/*
-const readline = require('readline');
-function readSyncByRl(tips) {
-    tips = tips || '> ';
-    return new Promise((resolve) => {
-        const rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout
-        });
-
-        rl.question(tips, (answer) => {
-            rl.close();
-            resolve(answer.trim());
-        });
-
-    });
+function getRandomNumber() {
+    let x= Math.floor(Math.random() * 9999 + 1000);
+    return x.toString();
 }
-readSyncByRl('请输入任意字符：').then((res) => {
-    console.log(res);
-});
-*/
-
+module.exports.guessNumber=guessNumber;
+module.exports.getRandomNumber=getRandomNumber;
 
