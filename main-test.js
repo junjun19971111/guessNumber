5ff5d3322f1681992f826436d0698f6c8e10b4a3
junjun <1121968314@qq.com>
2017-07-19 23:39:44 -0400

    ..

--- main-test.js
+++ main-test.js
@@ -1,76 +1,125 @@
+const sinon = require('sinon');
+const readlineSync = require('readline-sync');
+
 const guessNumberModule=require('./main');
-function testGuessNumber() {
-    //let number=sinon.stub(testNumber,guessNumber(num));
-    //number.withArgs(1234).return('1234');
+function theTestGuessNumber1() {
+    const randomNumber = sinon.stub(guessNumberModule, 'getRandomNumber').return('1234');
+    const inputGuessNumber = sinon.stub(readlineSync, 'question').return('5678');
+    const expectResult1 = '0A0B';
+    const result1 = guessNumberModule.guessNumber();
+    console.log(result1 === expectResult1 ? '1 test passed' : '1 test failed');
 
-    const number1='5678';
-    const expectResult1='0A0B';
-    const result1=guessNumberModule.guessNumber(number1);
-    console.log(result1===expectResult1?'1 test passed':'1 test failed');
-
-    const number2='5671';
-    const expectResult2='0A1B';
-    const result2=guessNumberModule.guessNumber(number2);
-    console.log(result2===expectResult2?'2 test passed':'2 test failed');
-
-    const number3='5621';
-    const expectResult3='0A2B';
-    const result3=guessNumberModule.guessNumber(number3);
-    console.log(result3===expectResult3?'3 test passed':'3 test failed');
-
-    const number4='5321';
-    const expectResult4='0A3B';
-    const result4=guessNumberModule.guessNumber(number4);
-    console.log(result4===expectResult4?'4 test passed':'4 test failed');
-
-    const number5='4321';
-    const expectResult5='0A4B';
-    const result5=guessNumberModule.guessNumber(number5);
-    console.log(result5===expectResult5?'5 test passed':'5 test failed');
-
-    const number6='1567';
-    const expectResult6='1A0B';
-    const result6=guessNumberModule.guessNumber(number6);
-    console.log(result6===expectResult6?'6 test passed':'6 test failed');
-
-    const number7='1452';
-    const expectResult7='1A1B';
-    const result7=guessNumberModule.guessNumber(number7);
-    console.log(result7===expectResult7?'7 test passed':'7 test failed');
-
-    const number8='1642';
-    const expectResult8='1A2B';
-    const result8=guessNumberModule.guessNumber(number8);
-    console.log(result8===expectResult8?'8 test passed':'8 test failed');
-
-    const number9='1423';
-    const expectResult9='1A3B';
-    const result9=guessNumberModule.guessNumber(number9);
-    console.log(result9===expectResult9?'9 test passed':'9 test failed');
-
-    const number10='1278';
-    const expectResult10='2A0B';
-    const result10=guessNumberModule.guessNumber(number10);
-    console.log(result10===expectResult10?'10 test passed':'10 test failed');
-
-    const number11='1248';
-    const expectResult11='2A1B';
-    const result11=guessNumberModule.guessNumber(number11);
-    console.log(result11===expectResult11?'11 test passed':'11 test failed');
-
-    const number12='1243';
-    const expectResult12='2A2B';
-    const result12=guessNumberModule.guessNumber(number12);
-    console.log(result12===expectResult12?'12 test passed':'12 test failed');
-
-    const number13='1235';
-    const expectResult13='3A0B';
-    const result13=guessNumberModule.guessNumber(number13);
-    console.log(result13===expectResult13?'13 test passed':'13 test failed');
-
-    const number14='1234';
-    const expectResult14='4A0B';
-    const result14=guessNumberModule.guessNumber(number14);
-    console.log(result14===expectResult14?'14 test passed':'14 test failed');
 }
-testGuessNumber();
+function theTestGuessNumber2() {
+    const inputGuessNumber = sinon.stub(readlineSync, 'question').return('5671');
+    const expectResult1 = '0A1B';
+    const result1 = guessNumberModule.guessNumber();
+    console.log(result1 === expectResult1 ? '2 test passed' : '2 test failed');
+
+}
+function theTestGuessNumber3() {
+    const inputGuessNumber = sinon.stub(readlineSync, 'question').return('5621');
+    const expectResult1 = '0A2B';
+    const result1 = guessNumberModule.guessNumber();
+    console.log(result1 === expectResult1 ? '3 test passed' : '3 test failed');
+
+}
+function theTestGuessNumber4() {
+    const inputGuessNumber = sinon.stub(readlineSync, 'question').return('5321');
+    const expectResult1 = '0A3B';
+    const result1 = guessNumberModule.guessNumber();
+    console.log(result1 === expectResult1 ? '4 test passed' : '4 test failed');
+
+}
+
+function theTestGuessNumber5() {
+    const inputGuessNumber = sinon.stub(readlineSync, 'question').return('4321');
+    const expectResult1 = '0A4B';
+    const result1 = guessNumberModule.guessNumber();
+    console.log(result1 === expectResult1 ? '5 test passed' : '5 test failed');
+
+}
+function theTestGuessNumber6() {
+    const inputGuessNumber = sinon.stub(readlineSync, 'question').return('1567');
+    const expectResult1 = '1A0B';
+    const result1 = guessNumberModule.guessNumber();
+    console.log(result1 === expectResult1 ? '6 test passed' : '6 test failed');
+
+}
+function theTestGuessNumber7() {
+    const inputGuessNumber = sinon.stub(readlineSync, 'question').return('1562');
+    const expectResult1 = '1A1B';
+    const result1 = guessNumberModule.guessNumber();
+    console.log(result1 === expectResult1 ? '7 test passed' : '7 test failed');
+
+}
+
+
+function theTestGuessNumber8() {
+    const inputGuessNumber = sinon.stub(readlineSync, 'question').return('1345');
+    const expectResult1 = '1A2B';
+    const result1 = guessNumberModule.guessNumber();
+    console.log(result1 === expectResult1 ? '8 test passed' : '8 test failed');
+
+}
+
+function theTestGuessNumber9() {
+    const inputGuessNumber = sinon.stub(readlineSync, 'question').return('1423');
+    const expectResult1 = '1A3B';
+    const result1 = guessNumberModule.guessNumber();
+    console.log(result1 === expectResult1 ? '9 test passed' : '9 test failed');
+
+}
+
+function theTestGuessNumber10() {
+    const inputGuessNumber = sinon.stub(readlineSync, 'question').return('1256');
+    const expectResult1 = '2A0B';
+    const result1 = guessNumberModule.guessNumber();
+    console.log(result1 === expectResult1 ? '10 test passed' : '10 test failed');
+
+}
+
+function theTestGuessNumber11() {
+    const inputGuessNumber = sinon.stub(readlineSync, 'question').return('1248');
+    const expectResult1 = '2A1B';
+    const result1 = guessNumberModule.guessNumber();
+    console.log(result1 === expectResult1 ? '11 test passed' : '11 test failed');
+
+}
+function theTestGuessNumber12() {
+    const inputGuessNumber = sinon.stub(readlineSync, 'question').return('1243');
+    const expectResult1 = '2A2B';
+    const result1 = guessNumberModule.guessNumber();
+    console.log(result1 === expectResult1 ? '12 test passed' : '12 test failed');
+
+}
+
+function theTestGuessNumber13() {
+    const inputGuessNumber = sinon.stub(readlineSync, 'question').return('1237');
+    const expectResult1 = '3A0B';
+    const result1 = guessNumberModule.guessNumber();
+    console.log(result1 === expectResult1 ? '13 test passed' : '13 test failed');
+
+}
+
+function theTestGuessNumber14() {
+    const inputGuessNumber = sinon.stub(readlineSync, 'question').return('1234');
+    const expectResult1 = '4A0B';
+    const result1 = guessNumberModule.guessNumber();
+    console.log(result1 === expectResult1 ? '14 test passed' : '14 test failed');
+
+}
+theTestGuessNumber1();
+theTestGuessNumber2();
+theTestGuessNumber3();
+theTestGuessNumber4();
+theTestGuessNumber5();
+theTestGuessNumber6();
+theTestGuessNumber7();
+theTestGuessNumber8();
+theTestGuessNumber9();
+theTestGuessNumber10();
+theTestGuessNumber11();
+theTestGuessNumber12();
+theTestGuessNumber13();
+theTestGuessNumber14();
